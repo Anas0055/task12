f6d781ee7eb7059feebb44f4a52fdf99aacf4db8
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:36:38 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+// Your Own Array: Think of your favorite mode of transportation,
+//  such as a motorcycle or a car, and make a list that stores 
+//  several examples. Use your list
+// to print a series of statements about these items, such as
+//  “I would like to own a Honda motorcycle.”
+
+
+let array= ["honda","civic","4x4","V8"]
+for( let a=0 ; a<4;a++){
+    console.log (`“I would like to own a ${array [a]} motorcycle.”
+    `)
+}
